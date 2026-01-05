0ad656de086d89ed5f60405177b7ce311e04abfa
swetha290605 <sevuganswetha@gmail.com>
2026-01-05 12:23:24 +0530

    Fourth Version

--- code1.py
+++ code1.py
@@ -4,4 +4,5 @@ n=input("Enter a number: ")
 n= int(n) #converted to integer
 for i in range(1,11):
     mul=n*i #calculation procedure
+    print("----displaying table-----")
     print(f"{n}*{i}= ", mul) #correctly formated Print Statement
