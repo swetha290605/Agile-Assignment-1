fdaebfcf9da7f97f971823543c7c9d75e79b97b5
Swetha <sevuganswetha@gmail.com>
2026-01-12 10:35:39 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,8 +1,5 @@
-# Code-Project-1
-#this is a multiplication table
 n=input("Enter a number: ")
-n= int(n) #converted to integer
+n= int(n) 
 for i in range(1,11):
-    mul=n*i #calculation procedure
-    print("----displaying table-----")
-    print(f"{n}*{i}= ", mul) #correctly formated Print Statement
+    mul=n*i 
+    print(f"{n}*{i}= ", mul)
